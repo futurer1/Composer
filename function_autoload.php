820dfa41bfc2b4b695ecfeae104ecd32b1aef50a
Michail <futurer@inbox.ru>
2017-07-20 19:20:10 +0300

    Description function __autoload()

--- function_autoload.php
+++ function_autoload.php
@@ -1,1 +1,15 @@
-
+/**
+* Функция вызывается автоматически при необходимости подключения нового класса.
+*/
+function __autoload($className)
+{
+    $class_pieces = explode("\\", $className);
+    switch($class_pieces[0]) {
+        case "tmp1":
+            require __DIR__ . "/../" . implode(DIRECTORY_SEPARATOR, $class_pieces) . ".php";
+            break;
+        case "tmp2":
+            require __DIR__ . "/../vendor/" . implode(DIRECTORY_SEPARATOR, $class_pieces) . ".php";
+            break;
+    }
+}
